c201455b95e20ad38d6258dfc37dd8f0d27285c4
vicky.phang <gitlab@dtc.best>
2024-10-25 08:26:41 +0700

    add openweathermap dag project

---
+++ open-weather/readme.md
@@ -0,0 +1,7 @@
+# Python ETL Pipeline with Airflow
+
+References: https://www.youtube.com/watch?v=uhQ54Dgp6To
+
+extract from API, transform using simple python using pandas, and load to S3 bucket in csv format.
+
+example of output file: current_weather_data_jakarta_17102024071129.csv
---
+++ open-weather/weather_dag.py
@@ -0,0 +1,105 @@
+from airflow import DAG
+from datetime import timedelta, datetime
+from airflow.providers.http.sensors.http import HttpSensor
+import json
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+import pandas as pd
+import boto3
+from botocore.client import Config
+
+
+def kelvin_to_fahrenheit(temp_in_kelvin):
+    temp_in_fahrenheit = (temp_in_kelvin - 273.15) * (9/5) + 32
+    return temp_in_fahrenheit
+
+
+def transform_load_data(task_instance):
+    data = task_instance.xcom_pull(task_ids="extract_weather_data")
+    city = data["name"]
+    weather_description = data["weather"][0]['description']
+    temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp"])
+    feels_like_farenheit= kelvin_to_fahrenheit(data["main"]["feels_like"])
+    min_temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp_min"])
+    max_temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp_max"])
+    pressure = data["main"]["pressure"]
+    humidity = data["main"]["humidity"]
+    wind_speed = data["wind"]["speed"]
+    time_of_record = datetime.utcfromtimestamp(data['dt'] + data['timezone'])
+    sunrise_time = datetime.utcfromtimestamp(data['sys']['sunrise'] + data['timezone'])
+    sunset_time = datetime.utcfromtimestamp(data['sys']['sunset'] + data['timezone'])
+
+    transformed_data = {"City": city,
+                        "Description": weather_description,
+                        "Temperature (F)": temp_farenheit,
+                        "Feels Like (F)": feels_like_farenheit,
+                        "Minimun Temp (F)":min_temp_farenheit,
+                        "Maximum Temp (F)": max_temp_farenheit,
+                        "Pressure": pressure,
+                        "Humidty": humidity,
+                        "Wind Speed": wind_speed,
+                        "Time of Record": time_of_record,
+                        "Sunrise (Local Time)":sunrise_time,
+                        "Sunset (Local Time)": sunset_time
+                        }
+    transformed_data_list = [transformed_data]
+    df_data = pd.DataFrame(transformed_data_list)
+
+    now = datetime.now()
+    dt_string = now.strftime("%d%m%Y%H%M%S")
+    dt_string = 'current_weather_data_jakarta_' + dt_string
+    df_data.to_csv(f"{dt_string}.csv", index=False)
+
+    # Create a boto3 session with custom S3 endpoint
+    s3 = boto3.client('s3', aws_access_key_id='XXX', aws_secret_access_key='XXX', endpoint_url='XXX', config=Config(s3={'addressing_style': 'path'}))
+
+    # Upload the CSV file to your custom S3 bucket
+    bucket_name = 'XXX'
+    s3.upload_file(f"{dt_string}.csv", bucket_name, f"{dt_string}.csv")
+
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 1, 8),
+    'email': ['myemail@domain.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=2)
+}
+
+
+
+with DAG('weather_dag',
+        default_args=default_args,
+        schedule_interval = '@daily',
+        catchup=False) as dag:
+
+
+        is_weather_api_ready = HttpSensor(
+        task_id ='is_weather_api_ready',
+        http_conn_id='weathermap_api',
+        endpoint='/data/2.5/weather?q=Jakarta&appid=f80804f0c67ab6a2b5149f03dac2f267'
+        )
+
+
+        extract_weather_data = SimpleHttpOperator(
+        task_id = 'extract_weather_data',
+        http_conn_id = 'weathermap_api',
+        endpoint='/data/2.5/weather?q=Jakarta&appid=f80804f0c67ab6a2b5149f03dac2f267',
+        method = 'GET',
+        response_filter= lambda r: json.loads(r.text),
+        log_response=True
+        )
+
+        transform_load_weather_data = PythonOperator(
+        task_id= 'transform_load_weather_data',
+        python_callable=transform_load_data
+        )
+
+
+
+
+        is_weather_api_ready >> extract_weather_data >> transform_load_weather_data
